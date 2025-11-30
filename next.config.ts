3d8b4a01ed98f808f37522e851cc48b99255087c
Mattias Puhk <mattias.puhk@gmail.com>
2025-11-30 11:19:20 +0100

    Fix deploy problems

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,25 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    // Ignore hardhat and contract files during build
+    config.externals = config.externals || [];
+    config.externals.push({
+      hardhat: "commonjs hardhat",
+      "@nomicfoundation/hardhat-toolbox":
+        "commonjs @nomicfoundation/hardhat-toolbox",
+    });
+
+    // Ignore .sol files and hardhat artifacts
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    config.module.rules.push({
+      test: /\.sol$/,
+      use: "ignore-loader",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
