f6b9faa2e72316ea687d5c5f236ec1d1688fa21e
Mattias Puhk <mattias.puhk@gmail.com>
2025-11-30 10:09:33 +0100

    Fix build issue v4

--- middleware.ts
+++ middleware.ts
@@ -13,4 +13,5 @@ export const config = {
   matcher: [
     "/((?!_next|api|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   ],
+  runtime: "nodejs",
 };
